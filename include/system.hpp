21445f5dc120e9d703c26d48c1e65752eb7af573
Preetham Jayaram <35173316+Preetham-J@users.noreply.github.com>
2019-11-08 19:41:22 -0500

    Modify the Shunting-Yard/RPN algorithm to work with internal Matrix and System classes.

--- include/system.hpp
+++ include/system.hpp
@@ -24,20 +24,24 @@ class System
 
         /* Add a matrix to the system. */
         void addMatrix(const std::string& key, int rows, int columns);
-        /* */
-        void parseComputation();
+        /* Add a matrix to using a generated key (for intermediate matrices in calculations) */ 
+        std::string addMatrix(const Matrix& matrix);
+        /* Initialize computation parsing and calculating */
+        void performComputation();
+        /* Calculates end value based on Reverse Polish Notation */
+        std::string parseComputation(const std::string& infix);
         /* Implements Shunting-Yard (Reverse Polish Notation) */
-        std::string calculate(const std::string& expression)
+        std::string calculate(const std::string& expression);
         /* Add two matrices. */
-        void add(const std::string& key_1, const std::string& key_2);
+        std::string add(const std::string& key_1, const std::string& key_2);
         /* Subtract two matrices. */
-        void subtract(const std::string& key_1, const std::string& key_2);
+        std::string subtract(const std::string& key_1, const std::string& key_2);
         /* Multiply two matrices. */
-        void multiplyMatrices(const std::string& key_1, const std::string& key_2);
+        std::string multiplyMatrices(const std::string& key_1, const std::string& key_2);
         /* Multiply a matrix and scalar. */
-        void multiplyByScalar(const std::string& key_1, const std::string& key_2);
+        std::string multiplyByScalar(const std::string& key_1, const std::string& key_2);
         /* Divice a matrix by a scalar. */
-        void divideByScalar(const std::string& key_1, const std::string& key_2);
+        std::string divideByScalar(const std::string& key_1, const std::string& key_2);
 
         /* Check if the system is running. */
         bool isRunning();
@@ -49,6 +53,7 @@ class System
     private:
         bool running = true;                                  // System running boolean
         std::unordered_map<std::string, Matrix> matrices;     // Stored system matrices
+        std::unordered_map<std::string, Matrix> computation_matrices;
 };
 
 
