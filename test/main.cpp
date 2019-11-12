ded39528d1c69dbb3099a878787349eae0367e36
Preetham-J <j.preetham42@gmail.com>
2019-11-11 22:25:58 -0500

    Add Google Test to CMake for unit testing.

---
+++ test/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * main.cpp
+ * Copyright (C) 2019 preetham <preetham@preetham-laptop>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
